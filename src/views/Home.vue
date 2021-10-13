8a2375a40a6cc7c5c1984803dabfcc6d68f4d974
zuoxiaobai <guoqzuo@gmail.com>
2021-10-14 07:47:09 +0800

    feat(test): test feat type commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <p>fix type commit test</p>
     <p>fix type commit test 2rd</p>
+    <p>feat type commit test</p>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
